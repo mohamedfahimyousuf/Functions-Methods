dbae8a3fea2853508777cd4c2cd69f5a5964ab0a
Mohamed Fahim M <129580983+mohamedfahimyousuf@users.noreply.github.com>
2025-01-29 11:00:21 +0530

    Create SumofNaturalNumbers.java

---
+++ SumofNaturalNumbers.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class SumOfNaturalNumbers {
+
+    public static int sumOfNaturalNumbers(int n) {
+        return (n * (n + 1)) / 2; 
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = sc.nextInt();
+        int sum = sumOfNaturalNumbers(n);
+        System.out.println("The sum of the first " + n + " natural numbers is: " + sum);
+    }
+}
