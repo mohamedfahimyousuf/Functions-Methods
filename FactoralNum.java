4d061b5abd42dedbc9c7d9c92705e80c6f17b838
Mohamed Fahim M <129580983+mohamedfahimyousuf@users.noreply.github.com>
2025-01-29 10:49:17 +0530

    Update FactoralNum.java

---
+++ FactoralNum.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class FactorialNum{
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a number: ");
+    int num = sc.nextInt();
+    System.out.println("Factorial of "+num+" is: "+factorial(num));
+    sc.close();
+    }
+    static int factorial(int num){
+        int product =1;
+        if(num ==1 || num ==0){
+            return 1;
+        }
+        else{
+            for(int i=1; i<=num; i++){
+                product = product*i;
+            }
+            return product;
+        }
+    }
+}
