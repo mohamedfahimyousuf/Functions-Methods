1cf21c8492257dd92d543385011b307c15c33fd1
Mohamed Fahim M <129580983+mohamedfahimyousuf@users.noreply.github.com>
2025-01-29 10:49:44 +0530

    Update Grades.java

---
+++ Grades.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class Grades
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Marks (out of 100): ");
+		int marks = sc.nextInt();
+		grades(marks);
+		sc.close();
+	}
+	static void grades(int marks){
+	    if(marks > 100){
+	        System.out.println("Enter the marks below 100");
+	    }
+	    else if(marks <= 40){
+	        System.out.println("Fail");
+	    }
+	    else if(marks>40 && marks<=50){
+	        System.out.println("Grade: DD");
+	    }
+	    else if(marks>50 && marks<=60){
+	        System.out.println("Grade: CD");
+	    }
+	    else if(marks>60 && marks<=70){
+	        System.out.println("Grade: BC");
+	    }
+	    else if(marks>70 && marks<=80){
+	        System.out.println("Grade: BB");
+	    }
+	    else if(marks>80 && marks<=90){
+	        System.out.println("Grade: AB");
+	    }
+	    else if(marks>90 && marks<=100){
+	        System.out.println("Grade: AA");
+	    }
+	    else{
+	        System.out.println("Enter Correct Input");
+	    }
+	}
+}
