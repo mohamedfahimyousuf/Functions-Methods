060d9cbb5d7d314db42a3dd83c19455456e61588
Mohamed Fahim M <129580983+mohamedfahimyousuf@users.noreply.github.com>
2025-01-29 10:50:38 +0530

    Update PrimeNum2.java

---
+++ PrimeNum2.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class PrimeNum2{
+    
+    static boolean isprime(int n){
+        
+        if(n<=1){
+            return false;
+        }
+        for(int i=2;i*i<=n;i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+    static void primenum(int start, int end){
+        for(int i = start;i<=end;i++){
+            if(isprime(i)){
+                System.out.println(i);
+            }
+        }
+    }
+    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Starting Number: ");
+        int start = sc.nextInt();
+        System.out.println("Enter the Ending Number: ");
+        int end = sc.nextInt();
+        System.out.println("Prime Numbers between "+start+"to "+end+" are: ");
+        primenum(start,end);
+        sc.close();
+    }      
+    }
