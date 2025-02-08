72b83153151fa9477c1ce3fce3a97aa0cebf04b1
Mohamed Fahim M <129580983+mohamedfahimyousuf@users.noreply.github.com>
2025-02-08 22:57:33 +0530

    Update MinAndMax.java

--- MinAndMax.java
+++ MinAndMax.java
@@ -4,11 +4,11 @@ class MinAndMax{
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Number 1: ");
+        System.out.println("Enter Number 1:");
         int num1 = sc.nextInt();
-        System.out.println("Enter Number 2: ");
+        System.out.println("Enter Number 2:");
         int num2 = sc.nextInt();
-        System.out.println("Enter Number 3: ");
+        System.out.println("Enter Number 3:");
         int num3 = sc.nextInt();
 
         int minimum = min(num1, num2, num3);
