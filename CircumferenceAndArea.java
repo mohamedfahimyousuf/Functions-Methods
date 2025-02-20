e950d330673fc1316f6dbeb5e546af368d5422f4
Mohamed Fahim M <129580983+mohamedfahimyousuf@users.noreply.github.com>
2025-02-20 20:15:22 +0530

    Update CircumferenceAndArea.java

--- CircumferenceAndArea.java
+++ CircumferenceAndArea.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class CircumferenceAndArea {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter Radius: ");
+        System.out.print("Enter Radius : ");
         Double radius = sc.nextDouble();
         Double circumference = circumferenceOfCircle(radius);
         Double area = area(radius);
