1c4d65c418a5c7784edbf0dbf803a963ddfbc43d
Mohamed Fahim M <129580983+mohamedfahimyousuf@users.noreply.github.com>
2025-02-25 23:57:57 +0530

    Update OddorEven.java

--- OddorEven.java
+++ OddorEven.java
@@ -3,7 +3,7 @@ public class OddorEven {
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number: ");
+        System.out.println("Enter a number : ");
         int num = sc.nextInt();
         if(iseven(num)){
             System.out.println("The number is even");
