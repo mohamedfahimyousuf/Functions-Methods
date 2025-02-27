d892698f42237c8ce3499a693d63d6ab45a75792
Mohamed Fahim M <129580983+mohamedfahimyousuf@users.noreply.github.com>
2025-02-27 23:16:48 +0530

    Update PalindromeNum.java

--- PalindromeNum.java
+++ PalindromeNum.java
@@ -3,7 +3,7 @@ import java.util.*;
 class PalindromeNum{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a Number: ");
+        System.out.println("Enter a Number : ");
         int num = sc.nextInt();
         palindrome(num);
         sc.close();
