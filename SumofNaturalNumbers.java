c0ec93b1b4803d00232bd77e4d398a176db2d787
Mohamed Fahim M <129580983+mohamedfahimyousuf@users.noreply.github.com>
2025-03-02 22:30:38 +0530

    Update SumofNaturalNumbers.java

--- SumofNaturalNumbers.java
+++ SumofNaturalNumbers.java
@@ -7,7 +7,7 @@ public class SumOfNaturalNumbers {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number: ");
+        System.out.println("Enter a number : ");
         int n = sc.nextInt();
         int sum = sumOfNaturalNum(n);
         System.out.println("The sum of the first " + n + " natural numbers is: " + sum);
